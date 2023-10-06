3517533ff6af85d4b463ea4bf4d991d65228744b
francisco <francisco.leyescampoy@gmail.com>
2023-10-06 18:07:08 -0300

    creando rutas dinamicas con next

--- coffeshop/src/app/page.jsx
+++ coffeshop/src/app/page.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Navbar from '@/components/Navbar/navbar';
+
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between p-2">
         {/* El contenido de tu página */}
       </main>
-    </div>
   );
 };
 
